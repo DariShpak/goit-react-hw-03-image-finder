cdce4f9908f230f46ba8b99b385ce8ec7ff32a2c
Daria Shpak <darya.shpak@gmail.com>
2023-07-22 00:48:37 +0300

    Update App.styled.js

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -7,4 +7,12 @@ export const Container = styled.div`
   justify-content: center;
 `
 
+export const Error = styled.div`
+text-align:center;
+margin-top:30px;
+`
+
+export const ErrorText = styled.h1`font-size: 36px;
+         color: #5b6988;`
+
 
